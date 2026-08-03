2571158635a98e148658b98f5be12446acb359d5
Pamodya Wijesinghe <wapamodyawijesinghe@gmail.com>
2026-08-03 16:36:38 +0530

    Remove base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/smart-breath-buddy-idealize/",
   server: {
     host: "::",
     port: 8080,
